70ee5ad0e510a53c62fddf38844833146fdebbdf
Debuty <Kareemsaleh84020@yahoo.com>
2025-07-15 13:59:18 +0300

    Change Text Fam

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,22 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Noto Sans Arabic', 'sans-serif'],
-        cairo: ['Cairo', 'sans-serif'],
+        // Default font for regular text
+        sans: ['Noto Sans Arabic', 'system-ui', 'sans-serif'],
+        // Cairo font for headings
+        cairo: ['Cairo', 'Noto Sans Arabic', 'sans-serif'],
+      },
+      fontSize: {
+        // Custom font sizes
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'base': ['1rem', { lineHeight: '1.5rem' }],
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '1' }],
       },
       spacing: {
         '18': '4.5rem',
